e32add771128aa9ad93dd258ec7c92d557a2387d
marcelosperalta <marcelosperalta@gmail.com>
2020-09-27 17:08:21 +0200

    portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,27 @@
-const home1 = document.querySelector('.h1-one');
-const home2 = document.querySelector('.h1-two');
 const div1 = document.querySelector('.div1');
 const div2 = document.querySelector('.div2');
 const div3 = document.querySelector('.div3');
 const div4 = document.querySelector('.div4');
 
-function typeWriter(element1, element2, element3, element4) {
-    const lettersArray1 = element1.innerHTML.split('');
-    const lettersArray2 = element2.innerHTML.split('');
-    const lettersArray3 = element3.innerHTML.split('');
-    element1.innerHTML = '';
-    element2.innerHTML = '';
-    element3.innerHTML = '';
-    // element4.innerHTML = '';
+function typeWriter(div1, div2, div3, div4) {
+    const lettersArray1 = div1.innerHTML.split('');
+    const lettersArray2 = div3.innerHTML.split('');
+    div1.innerHTML = '';
+    div3.innerHTML = '';
+    div4.style.display = 'none';
     lettersArray1.forEach(function(letter, index) {
             setTimeout(function(){
-                element1.innerHTML += letter;
-                if (element1.innerHTML === "Hi, I'm Marcelo Soares Peralta.") {
+                div1.innerHTML += letter;
+                if (div1.innerHTML === "Hi, I'm Marcelo Soares Peralta.") {
                     lettersArray2.forEach(function(letter, index) {
                         setTimeout(function(){
-                            element4.style.display = "none";
-                            element2.style.display = "block";
-                            element2.innerHTML += letter;
+                            div2.style.display = "none";
+                            div4.style.display = "block";
+                            div3.innerHTML += letter;
                         }, 120 * index)
                     });
                 }
             }, 120 * index)
     });
-    lettersArray3.forEach(function(letter, index) {
-        setTimeout(function(){
-            element3.innerHTML += letter;
-        }, 120 * index)
-    });
 }
-typeWriter(home1, home2, div1, div2, div3, div4);
+typeWriter(div1, div2, div3, div4);
